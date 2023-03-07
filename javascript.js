0791620e0e11b4e81b49ee1db7ccb8ece541e23f
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-03-07 18:43:18 +0100

    add result code, almost working

--- javascript.js
+++ javascript.js
@@ -1,28 +1,51 @@
-function add(a, b) {
-    return a + b
-}
-
-function substract(a, b) {
-    return a - b
-}
-
-function multiply(a, b) {
-    return a * b
-}
-
-function divide(a, b) {
-    return a / b
-}
-
 const buttons = document.querySelectorAll("button")
 const display = document.querySelector("#display")
 
-
+let inputValue1 = ""
+let inputValue2 = ""
+let option = ""
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        const inputValue = button.id
-        display.textContent = inputValue
-    })
-});
+        if (option === "") {
+            inputValue1 += button.id
+            display.textContent = inputValue1
+        } else {
+            inputValue2 += button.id
+            display.textContent = inputValue2
 
+        }
+
+        if (option !== "result") {
+            option += button.className
+            display.textContent = ""
+            console.log(inputValue1)
+        }
+         else {
+            console.log (inputValue2)
+         }
+        
+        switch (option) {
+        case "add":
+        display.textContent = (parseInt(inputValue1) + parseInt(inputValue2))
+        console.log (parseInt(inputValue1) + parseInt(inputValue2))
+        break;
+        case "substract":
+        display.textContent = (parseInt(inputValue1) - parseInt(inputValue2))
+        console.log (parseInt(inputValue1) - parseInt(inputValue2))
+        break;
+        case "divide":
+        display.textContent = (parseInt(inputValue1) / parseInt(inputValue2))
+        console.log (parseInt(inputValue1) / parseInt(inputValue2))
+        break;
+        case "multiply":
+        display.textContent = (parseInt(inputValue1) * parseInt(inputValue2))
+        console.log (parseInt(inputValue1) * parseInt(inputValue2))
+        }
+
+
+    })
+
+
+
+});
